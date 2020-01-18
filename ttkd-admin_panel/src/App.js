4b2cea7adc2ea37b33fe4355e0f742addd88d965
Marianna Sternefeld <marianna@sternefeld.com>
2020-01-18 18:39:00 -0500

    Got Bootstrap working

--- ttkd-admin_panel/src/App.js
+++ ttkd-admin_panel/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Header} from "./components/header";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
