b00769dd7aaa976cc2dba1a65adfa6cc38265b69
Marianna Sternefeld <marianna@sternefeld.com>
2020-01-23 10:23:36 -0500

    Added some fixes for the auth

--- ttkd-admin_panel/src/components/pages/Homepage.js
+++ ttkd-admin_panel/src/components/pages/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from '../../../public/assets/btn_google_signin_light_normal_web@2x.png'
 import { Button, Form } from 'react-bootstrap';
 
 class Homepage extends React.Component {
