e205f7e9b3a212e205ac1c93d21c8a9daee088ae
Angela Bamfield <angelabamfield@gmail.com>
2020-01-29 20:04:38 -0500

    Added Express Backend
    http://localhost:3000/list is the example
    
    Tutorial:
    https://dev.to/nburgess/creating-a-react-app-with-react-router-and-an-express-backend-33l3

---
+++ client/src/components/pages/index.js
@@ -0,0 +1,7 @@
+import Homepage from "./Homepage.js";
+import List from "./List.js";
+
+export {
+    Homepage,
+    List
+}
--- ttkd-admin_panel/src/components/pages/index.js
+++
@@ -1,6 +0,0 @@
-import Homepage from "./Homepage.js";
-
-export {
-    Homepage,
-    List
-}
