29eb147f7f6a11cdb4dd9d8e178114b3238d6412
Isaiah Ocasio <ijo5372@g.rit.edu>
2020-02-20 16:54:44 -0500

    Adding the yarn.lock ad package.json files.

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,25 +1,25 @@
-// import Homepage from "./Homepage.js";
-// import List from "./List.js";
-// import Dashboard from "./Dashboard.js";
-// import FileStructure from "./FileStructure.js";
-// import Audio from "./Audio.js";
-// import Steps from "./Steps.js";
-// import Video from "./Video.js";
+import Homepage from "./Homepage.js";
+import List from "./List.js";
+import Dashboard from "./Dashboard.js";
+import FileStructure from "./FileStructure.js";
+import Audio from "./Audio.js";
+import Steps from "./Steps.js";
+import Video from "./Video.js";
 
-// export {
-//     Homepage,
-//     List,
-//     Dashboard, 
-//     FileStructure,
-//     Audio, 
-//     Steps, 
-//     Video
-// }
+export {
+    Homepage,
+    List,
+    Dashboard, 
+    FileStructure,
+    Audio, 
+    Steps, 
+    Video
+}
 
-export { default as Homepage } from './Homepage';
-export { default as Audio } from './Audio';
-export { default as Dashboard } from './Dashboard';
-export { default as FileStructure } from './FileStructure';
-export { default as List } from './List';
-export { default as Steps } from './Steps';
-export { default as Video } from './Video';
+// export { default as Homepage } from './Homepage';
+// export { default as Audio } from './Audio';
+// export { default as Dashboard } from './Dashboard';
+// export { default as FileStructure } from './FileStructure';
+// export { default as List } from './List';
+// export { default as Steps } from './Steps';
+// export { default as Video } from './Video';
