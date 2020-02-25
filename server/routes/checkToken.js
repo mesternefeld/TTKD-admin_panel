8d145d26e1a5d3ddd0e3650a4c90b1de4b53669b
Marianna Sternefeld <marianna@sternefeld.com>
2020-02-24 22:13:30 -0500

    Added the token information. Now things are verified

--- server/routes/checkToken.js
+++ server/routes/checkToken.js
@@ -7,22 +7,20 @@ var router = express.Router();
 router.post('/', (req, res, next) => {
     //res.json(videos);
     //res.send(categories);
+    var CLIENT_ID = "402862016858-3316mp00ucrloj1fih46qmf1dgf6cdh8.apps.googleusercontent.com";
     console.log(req.body);
-    //console.log(res);
-    /*const client = new OAuth2Client(CLIENT_ID);
+    const client = new OAuth2Client(CLIENT_ID);
     async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    });
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-    // If request specified a G Suite domain:
-    //const domain = payload['hd'];
-    }*/
-    //verify().catch(console.error);
+        const ticket = await client.verifyIdToken({
+            idToken: req.body["id_token"],
+            audience: CLIENT_ID,
+        });
+        console.log(ticket);
+        const payload = ticket.getPayload();
+        console.log(payload);
+    }
+    var payload = verify().catch(console.error);
+    console.log(payload);
     res.send({ status: 'SUCCESS' });
     console.log('[INFO][SERVER][API: /checkToken] Checking Token: ');
 });
